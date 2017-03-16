eb936340ebfefaa9dfe25136f86b174f75f9eefd
Horace Webb <horacewebb95@gmail.com>
2017-03-16 10:37:11 +0800

    添加异步方法

--- HostForTest/Form1.cs
+++ HostForTest/Form1.cs
@@ -3,6 +3,17 @@ using System.Data;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.Windows.Forms;
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.ComponentModel;
+using System.Threading;
+
 
 
 
@@ -56,10 +67,10 @@ namespace BackStageSur
     
     public class cl : Icl
     {
-        public const string connstr = "Server=127.0.0.1;Port=5432;Database=BackStageSur;Uid=postgres;Pwd=4770;";
-        public int Login(string p,string pswd)
+        public const string connstr = "Server=172.16.30.145;Port=9620;Database=BackStageSur;Uid=postgres;Pwd=swjtu;";
+        public int Login(string p,string pswd)//登录方法
         {
-            string sqlstrlgn = "select passwd from ser.tb_login where cleintid='" + p + "'";
+            string sqlstrlgn = "select passwd from ser.tb_login where cleintid='" + p + "'";//选择clientid相对应的MD5
             Npgsql.NpgsqlConnection myconnlgn = new Npgsql.NpgsqlConnection(connstr);
             Npgsql.NpgsqlCommand mycommlgn = new Npgsql.NpgsqlCommand(sqlstrlgn, myconnlgn);
 
@@ -69,14 +80,14 @@ namespace BackStageSur
             //DataSet ds = new DataSet();
             //myda.Fill(dt);
             //ds.Tables.Add(dt);
-            string comp = mycommlgn.ExecuteScalar().ToString();
+            string comp = mycommlgn.ExecuteScalar().ToString();  // TODO:test  //MD5赋值给临时变量
             myconnlgn.Close();
             myconnlgn.Dispose();
-            if (comp == "" || comp == null)
+            if (comp == "" || comp == null)//判断是否有对应MD5
             {
                 return 2;
             }
-            else if (comp == pswd)
+            else if (comp == pswd)//判断相等
             {
                 return 0;
             }
@@ -85,7 +96,7 @@ namespace BackStageSur
         }
        
 
-        public DataSet GetServer(string p)
+        public DataSet GetServer(string p)//服务器列表方法
         {
             string sqlstrGtSer = "select serverid,name,type,url from ser.tb_server where cleintid='" + p + "'";
             Npgsql.NpgsqlConnection myconnGtSer = new Npgsql.NpgsqlConnection(connstr);
@@ -98,7 +109,152 @@ namespace BackStageSur
             dsGtSer.Tables.Add(dtGtSer);
             return dsGtSer;
         }
-        
+        public int PingSer(string serid,IPAddress Address,ref long RtT,ref int Ttl,bool DF,ref int BfL)//同步Ping
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+           
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send(Address, timeout, buffer, options);// args[0] can be an IPaddress or host name.
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                RtT = reply.RoundtripTime;
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Ttl = reply.Options.Ttl;
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                DF = reply.Options.DontFragment;
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+                BfL = reply.Buffer.Length;
+#region  向数据库写入数据
+                string MetData = "INSERT INTO ser.data(serverid, rtt, ttl, df, bfl, \"time\", )VALUES(@serverid, @rtt, @ttl, @df, @bfl, @time ) ; ";
+                Npgsql.NpgsqlConnection myconnping = new Npgsql.NpgsqlConnection(connstr);
+                Npgsql.NpgsqlCommand mycommping = new Npgsql.NpgsqlCommand(data, myconnping);
+                myconnping.Open();
+                //try
+                //{
+                    mycommping.Parameters.Add("@serverid", NpgsqlTypes.NpgsqlDbType.Numeric).Value = serid;
+                    mycommping.Parameters.Add("@rtt", NpgsqlTypes.NpgsqlDbType.Bigint).Value = RtT;
+                    mycommping.Parameters.Add("@ttl", NpgsqlTypes.NpgsqlDbType.Integer).Value = Ttl;
+                    mycommping.Parameters.Add("@df", NpgsqlTypes.NpgsqlDbType.Boolean).Value = DF;
+                    mycommping.Parameters.Add("@bfl", NpgsqlTypes.NpgsqlDbType.Integer).Value = BfL;
+                    mycommping.Parameters.Add("@time", NpgsqlTypes.NpgsqlDbType.Timestamp).Value = DateTime.Now.ToLongTimeString();
+                    mycommping.ExecuteNonQuery();
+                    myconnping.Close();
+                //}
+
+
+#endregion
+
+
+
+
+                return 0;
+            }
+            else
+                return 1;
+        }
+        public static void PingSerAsync(string[] args)
+        {
+            if (args.Length == 0)
+                throw new ArgumentException("Ping needs a host or IP Address.");
+
+            string who = args[0];
+            AutoResetEvent waiter = new AutoResetEvent(false);
+
+            Ping pingSender = new Ping();
+
+            // When the PingCompleted event is raised,
+            // the PingCompletedCallback method is called.
+            pingSender.PingCompleted += new PingCompletedEventHandler(PingCompletedCallback);
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+
+            // Wait 12 seconds for a reply.
+            int timeout = 12000;
+
+            // Set options for transmission:
+            // The data can go through 64 gateways or routers
+            // before it is destroyed, and the data packet
+            // cannot be fragmented.
+            PingOptions options = new PingOptions(64, true);
+
+            Console.WriteLine("Time to live: {0}", options.Ttl);
+            Console.WriteLine("Don't fragment: {0}", options.DontFragment);
+
+            // Send the ping asynchronously.
+            // Use the waiter as the user token.
+            // When the callback completes, it can wake up this thread.
+            pingSender.SendAsync(who, timeout, buffer, options, waiter);
+
+            // Prevent this example application from ending.
+            // A real application should do something useful
+            // when possible.
+            waiter.WaitOne();
+            Console.WriteLine("Ping example completed.");
+        }
+
+        private static void PingCompletedCallback(object sender, PingCompletedEventArgs e)
+        {
+            // If the operation was canceled, display a message to the user.
+            if (e.Cancelled)
+            {
+                Console.WriteLine("Ping canceled.");
+
+                // Let the main thread resume. 
+                // UserToken is the AutoResetEvent object that the main thread 
+                // is waiting for.
+                ((AutoResetEvent)e.UserState).Set();
+            }
+
+            // If an error occurred, display the exception to the user.
+            if (e.Error != null)
+            {
+                Console.WriteLine("Ping failed:");
+                Console.WriteLine(e.Error.ToString());
+
+                // Let the main thread resume. 
+                ((AutoResetEvent)e.UserState).Set();
+            }
+
+            PingReply reply = e.Reply;
+
+            DisplayReply(reply);
+
+            // Let the main thread resume.
+            ((AutoResetEvent)e.UserState).Set();
+        }
+
+        public static void DisplayReply(PingReply reply)
+        {
+            if (reply == null)
+                return;
+
+            Console.WriteLine("ping status: {0}", reply.Status);
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+            }
+        }
+        //public DataSet SeqPingSer (DataSet s)
+        //{
+
+        //}
     }
 
 
@@ -109,5 +265,6 @@ namespace BackStageSur
         
         int Login(string p, string pswd);
         DataSet GetServer(string p);
+        int PingSer(IPAddress Address, ref long RtT, ref int Ttl, bool DF, ref int BfL);
     }
 }
